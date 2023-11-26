b337f18bed9ca2e147b6e15aa15992ceca858df7
Madison Ell <54490369+mell00@users.noreply.github.com>
2023-11-26 14:50:18 -0800

    added an error catch to fix bug, more fixes needed

--- participants.r
+++ participants.r
@@ -1,17 +1,23 @@
 library(rvest)
 
-# URL of the webpage to scrape
-page_url <- "https://wiki.santafe.edu/index.php/Complex_Systems_Summer_School_2018_(CSSS)"
+scrape_participants <- function(page_url){
+  
+  tryCatch({
+  # Read the HTML content of the page
+  page <- read_html(page_url)
+  
+  # CSS selector to find the hyperlink by its label
+  xpath_selector <- '//a[text()="Participants"]'
+  
+  # Extract the link
+  participant_link <- page %>%
+    html_node(xpath = xpath_selector) %>%
+    html_attr('href')
+  
+  print(participant_link)}, 
+  
+  error = function(e) {
+    return(NA)  # Return NA in case of an error
+  })}
 
-# Read the HTML content of the page
-page <- read_html(page_url)
-
-# CSS selector to find the hyperlink by its label
-xpath_selector <- '//a[text()="Participants"]'
-
-# Extract the link
-participant_link <- page %>%
-  html_node(xpath = xpath_selector) %>%
-  html_attr('href')
-
-print(participant_link)
+scrape_participants(page_url)
