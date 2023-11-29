2b69b9222ce8d26327420584974252b057af57f6
Madison Ell <54490369+mell00@users.noreply.github.com>
2023-11-29 10:52:53 -0800

    working on main data frame that mirrors that of spreadsheet data

--- main.r
+++ main.r
@@ -2,3 +2,7 @@ library(httr)
 library(jsonlite)
 library(rvest)
 library(dplyr)
+
+main_df <- data.frame(Year = c(1), Name = character(1), 
+                      Affiliation = character(1), Country = character(1),
+                      )
