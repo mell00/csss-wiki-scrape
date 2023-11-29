c466a14e01a69de88e5f6c89ee38039a0592065a
Madison Ell <54490369+mell00@users.noreply.github.com>
2023-11-29 10:52:00 -0800

    added working participant name/link extraction functions, still need to apply to all participant page links at once

--- participant_loop.r
+++ participant_loop.r
@@ -14,6 +14,63 @@ participants_links <- sapply(links_df$URLs, scrape_participants, USE.NAMES = FAL
 participants_links <- sapply(participants_links, make_absolute_url)
 
 # The result is a vector of "Participants" links
+names(participants_links) = NULL
 print(participants_links)
+
+
+
+#-------------------------------------------------------------------------------------------
+
+#Works for 1 - 10, 13, 14
+
+scrape_names_1 <- function(html_content) {
+  # Convert the HTML string to an HTML document
+  page <- read_html(html_content)
+  
+  # Extract the names (enclosed in <b> tags within <p> tags)
+  names <- page %>% html_nodes("p > b") %>% html_text(trim = TRUE)
+  
+  # Create a dataframe with the names
+  data <- data.frame(Name = names, stringsAsFactors = FALSE)
+  
+  return(data)
+}
+
+
+# Works for all other years
+
+scrape_participants_info_2 <- function(html_content) {
+  # Convert the HTML string to an HTML document
+  page <- read_html(html_content)
+  
+  # Extract the names and links (enclosed in <a> tags within <p> tags)
+  participant_nodes <- page %>% html_nodes("p > a")
+  names <- participant_nodes %>% html_text(trim = TRUE)
+  links <- participant_nodes %>% html_attr("href")
+  abs_links <- sapply(links, make_absolute_url)
+  names(abs_links) <- NULL
+  
+  # Create a dataframe with the names and links
+  data <- data.frame(Name = names, Link = abs_links, stringsAsFactors = FALSE)
+  
+  
+  # Exclude entries where Name is actually a link or Link does not contain 'santafe.edu'
+  valid_entries <- !grepl("http", data$Name, ignore.case = TRUE) & grepl("santafe.edu", data$Link, ignore.case = TRUE)
+  data <- data[valid_entries, ]
+  
+  # Remove non-alphanumeric characters from all names
+  data$Name <- gsub("[^[:alnum:] ]", "", data$Name)
+  
+  # Reset row indices
+  row.names(data) <- NULL
+  
+  return(data)
+}
+
+#scrape_names_1(participants_links[10])
+
+#scrape_participants_info_2(participants_links[15])
+
+#participants_links[11]
 
 
